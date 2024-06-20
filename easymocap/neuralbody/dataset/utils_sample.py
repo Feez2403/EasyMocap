fix depth computation single instances prune alpha<0.5

easymocap/neuralbody/dataset/utils_sample.py
@@ -17,7 +17,7 @@ def get_rays(H, W, K, R, T):
     # calculate the ray direction
     rays_d = pixel_world - rays_o[None, None]
     # ATTN: dont't normalize here
-    # rays_d = rays_d / np.linalg.norm(rays_d, axis=-1, keepdims=True)
+    rays_d = rays_d / np.linalg.norm(rays_d, axis=-1, keepdims=True)
     rays_o = np.broadcast_to(rays_o, rays_d.shape)
     rays_o = rays_o.astype(np.float32)
     rays_d = rays_d.astype(np.float32)

easymocap/neuralbody_relight/dataset/utils_sample.py
@@ -17,7 +17,7 @@ def get_rays(H, W, K, R, T):
     # calculate the ray direction
     rays_d = pixel_world - rays_o[None, None]
     # ATTN: dont't normalize here
-    # rays_d = rays_d / np.linalg.norm(rays_d, axis=-1, keepdims=True)
+    rays_d = rays_d / np.linalg.norm(rays_d, axis=-1, keepdims=True)
     rays_o = np.broadcast_to(rays_o, rays_d.shape)
     rays_o = rays_o.astype(np.float32)
     rays_d = rays_d.astype(np.float32)
@@ -579,6 +579,7 @@ def create_cameras_mean(cameras, camera_args):
     print('[sample] camera centers: ', center[0].T[0])
     print('[sample] camera radius: ', radius)
     print('[sample] camera start theta: ', theta0)
+    print('[sample] camera angle with ground: ', np.arccos(zmean), np.rad2deg(np.arccos(zmean)))
     Tnew = -np.einsum('bmn,bnp->bmp', Rnew, loc)
     K = Kold.mean(axis=0, keepdims=True).repeat(Tnew.shape[0], 0)
     return K, Rnew, Tnew

easymocap/neuralbody_relight/model/nerfplus.py
@@ -50,11 +50,15 @@ class NeRFT(Nerf):
 
     def calculate_density(self, wpts, **kwargs):
         latents = {'time': self.cache['embed']}
+        #print ("wpts", wpts.shape, wpts.requires_grad)
         return super().calculate_density(wpts, latents, **kwargs)
     
     def gradient(self, wpts, **kwargs):
         outputs = self.calculate_density(wpts, **kwargs)
-        return torch.autograd.grad(outputs['occupancy'], wpts, create_graph=True)[0]
+        #print ("occupancy", outputs['occupancy'].shape)
+        #print (outputs['occupancy'].requires_grad)
+        with torch.no_grad():
+            return torch.autograd.grad(outputs['occupancy'], wpts, torch.ones_like(outputs['occupancy']), create_graph=True)[0]
     
     def get_feature(self, wpts, sparse_feature):
         # feature_volume = self.encode_sparse_voxels(sp_input)

easymocap/neuralbody_relight/model/neuralbody.py
@@ -260,9 +260,13 @@ class Network(Nerf):
         valid = valid.all(dim=-1)
         if valid.sum() == 0:
             # no points inside the bbox
-            #print('self.current : No points inside the bbox')  
+            print('self.current : No points inside the bbox')
+            print(ppts[:10,:])
+            print("min", sparse_feature['sp_input']['min_xyz'])
+            print("max", sparse_feature['sp_input']['max_xyz']) 
+            print("valid.sum() == 0")
             outputs = {
-                'occupancy': torch.zeros((*wpts.shape[:-1], 1), device=wpts.device, dtype=wpts.dtype)
+                'occupancy': torch.zeros((*wpts.shape[:-1], 1), device=wpts.device, dtype=wpts.dtype, requires_grad=wpts.requires_grad),
             }
             outputs['density'] = outputs['occupancy']
             return outputs
@@ -275,15 +279,23 @@ class Network(Nerf):
         outputs = super().calculate_density(ppts_inlier, 
             extra_density=xyzc_features, latents={'time': sparse_feature['latent_time'][0]})
         outputs_all = {}
+        
         for key, val in outputs.items():
+            #print (key, val.shape, val.requires_grad)
             padding = torch.zeros((wpts_flat.shape[0], val.shape[-1]), device=val.device, dtype=val.dtype)
             padding[valid] = val
+            #print (key, "pad", padding.shape, padding.requires_grad)
             outputs_all[key] = padding.view(*wpts.shape[:-1], val.shape[-1])
+            #print (key, "out_all", outputs_all[key].shape, outputs_all[key].requires_grad)
         return outputs_all
     
     def gradient(self, wpts, **kwargs):
         outputs = self.calculate_density(wpts, **kwargs)
-        return torch.autograd.grad(outputs['occupancy'], wpts, create_graph=True)[0]
+        #print ("occupancy", outputs['occupancy'].shape)
+        #print (outputs['occupancy'].requires_grad)
+        with torch.no_grad():
+            return torch.autograd.grad(outputs['occupancy'], wpts, torch.ones_like(outputs['occupancy']), create_graph=True, allow_unused=True)[0]
+            
         
         
     

easymocap/neuralbody_relight/renderer/render_base.py
@@ -79,25 +79,31 @@ def raw2outputs(outputs, z_vals, rays_d, bkgd=None):
             -1), -1)[:, :-1]
     acc_map = torch.sum(weights, -1)
     # ATTN: here depth must /||ray_d||
-    depth_map = torch.sum(weights * z_vals, -1)/(1e-10 + acc_map)/torch.norm(rays_d, dim=-1).squeeze() # [N_rays]
-    
     # OPTION 1 : weighted average of the depth from the raw output 
-    #depth_raw = torch.sum(weights * z_vals, -1)/torch.norm(rays_d, dim=-1).squeeze() # [N_rays]
-    depth_raw = depth_map
+    depth_raw = torch.sum(weights * z_vals, -1)/torch.norm(rays_d, dim=-1).squeeze() # [N_rays]
+    depth_map = torch.sum(weights * z_vals, -1)/(1e-10 + acc_map)/torch.norm(rays_d, dim=-1).squeeze() # [N_rays]
     
     # OPTION 2 : first value of z_vals above the threshold
     #threshold = 0.5
     #msk = torch.gt(alpha, threshold)
+    ##first value of z_vals above the threshold
+    #indices = torch.argmax(msk.to(torch.long), dim=-1) # argmax gives the first True value
     #
-    #indices = torch.argmax(msk, dim=-1)
-    #depth_raw = torch.where(msk.any(dim=-1), z_vals[torch.arange(z_vals.shape[0]), indices], torch.zeros_like(z_vals[:, 0])) / torch.norm(rays_d, dim=-1).squeeze()
+    #depth_map = z_vals.gather(-1, indices.unsqueeze(-1)).squeeze(-1)
+    #
+    #depth_map = depth_map/ (1e-10 + acc_map)/ torch.norm(rays_d, dim=-1).squeeze()
+    #
+    #instances = outputs['instance']
+    #print(f"instances.shape: {instances.shape}")
+    #inst_map = instances.gather(-1, indices.unsqueeze(-1)).squeeze(-1)
     
-     
+    depth_raw = depth_map
     
     results = {
         'acc_map': acc_map, # [N_rays]
         'depth_map': depth_map, # [N_rays]
         'depth_raw': depth_raw, # [N_rays]
+        #'instance_map': inst_map, # [N_rays]
     }
     for key, val in outputs.items():
         if key == 'occupancy':
@@ -129,7 +135,13 @@ class BaseRenderer(nn.Module):
     def compose(self, retlist, mask=None, bkgd=None):
         res = {}
         for key in retlist[0].keys():
+            
             val = torch.cat([r[key] for r in retlist])
+            #if key == 'surf':
+            #    print(f"SURF")
+            #    print(f"val.shape: {val.shape}")
+            #    print(f"requires_grad: {val.requires_grad}")
+            #    print(f"requires_grad: {[r[key].requires_grad for r in retlist]}")
             if mask is not None and val.shape[0] != mask.shape[0]:
                 val_ = torch.zeros((mask.shape[0], *val.shape[1:]), device=val.device, dtype=val.dtype)
                 if key == 'rgb_map': # consider the background
@@ -205,6 +217,8 @@ class BaseRenderer(nn.Module):
                     #    if param.requires_grad:
                     #        print(f"param: {param}")
                     density = raw_output['occupancy']
+                    #print ("pts_grad: ", pts.requires_grad)
+                    #print ("density_grad: ", density.requires_grad)
                     normal = -torch.autograd.grad(density, pts, torch.ones_like(density), retain_graph=True)[0] 
                     raw_output['normal'] = normal 
                     #print(f"raw_output.keys: {raw_output.keys()}") # "occupancy", "raw_alpha", "density", "normal
@@ -271,13 +285,47 @@ class BaseRenderer(nn.Module):
         #print (f"depth.shape: {depth.shape}")
         #print (f"ray_d.shape: {ray_d.shape}")
         #print (f"ray_o.shape: {ray_o.shape}")   
+        #print ("depth_raw_shape: ", depth.shape)
+        #print ("ray_o_shape: ", ray_o.shape)
+        #print ("ray_d_shape: ", ray_d.shape)
         surf = ray_o + ray_d * depth.reshape(-1,1,1) # (N_rays,1, 3) xyz of the surface
-        
+        #print (f"surf.shape: {surf.shape}")
+        #print (surf.requires_grad)
         with torch.no_grad():
             ret['normal_map'] = torch.nn.functional.normalize(ret['normal_map'], p=2, dim=-1) # (N_rays, 3) normal of the surface
             normal = ret['normal_map']
             
             #normal = torch.nn.functional.normalize(ret['normal_map'], p=2, dim=-1)
+            
+           
+        instance_map = ret["instance_map"]
+        argmax_instances = torch.argmax(instance_map, dim=-1)
+        alpha_map = ret["acc_map"]
+        
+        
+        for i, key in enumerate(object_keys):
+            if key in ["ground", "background"]:
+                continue
+            model__ = self.net.model(key)
+            mask_surf = (argmax_instances == i) & (alpha_map > 0.5)
+            if mask_surf.sum() == 0:
+                continue
+            #print("key", key)
+            #print (f"mask_surf.shape: {mask_surf.shape}")
+            #print (f"mask_surf.requires_grad: {mask_surf.requires_grad}")    
+            surf_layer = surf.squeeze(-2)[mask_surf].unsqueeze(0)
+            #print (f"surf_layer.shape: {surf_layer.shape}")
+            #print (f"surf_layer.requires_grad: {surf_layer.requires_grad}")
+            normal_pred = model__.gradient(surf_layer)
+            if normal_pred is None:
+                continue
+            normal_pred = -torch.nn.functional.normalize(normal_pred.detach(), p=2, dim=-1, eps=1e-7)
+            normal[mask_surf] = normal_pred
+            
+            ret['normal_map'][mask_surf] = normal_pred
+                #surf.reshape(n_pixel, 3)
+    
+        
         if self.split == 'train' :
             with torch.no_grad():
         
@@ -451,13 +499,13 @@ class BaseRenderer(nn.Module):
         retlist = []
         for bn in range(0, viewdir.shape[0], self.chunk):
             
-            print(f"bn: {bn} / {viewdir.shape[0]}")
+            #print(f"bn: {bn} / {viewdir.shape[0]}")
             start, end = bn, min(bn + self.chunk, viewdir.shape[0])
             ret = self.batch_forward(batch, viewdir, start, end, bkgd)
             if ret is not None:
                 if self.split != 'train':
                     for key in ret.keys():
-                        if isinstance(ret[key], torch.Tensor):
+                        if isinstance(ret[key], torch.Tensor) :
                             ret[key] = ret[key].detach()
                         
                 retlist.append(ret)
@@ -466,7 +514,7 @@ class BaseRenderer(nn.Module):
         #print(f"rgb.shape: {rgb.shape}")
             
         res = self.compose(retlist)
-        
+        #print ("res_surf_grad: ", res["surf"].requires_grad)
         #if batch["meta"]["step"] % 10 == 0:
         #    coords = batch['coord']
 #

easymocap/neuralbody_relight/renderer/render_relight.py
@@ -72,12 +72,12 @@ class RelightModule(nn.Module):
         
         self.predict_normals = True
         
-        self.predict_normals_lvis_only = True
+        self.predict_normals_lvis_only = False
         
         if self.predict_normals_lvis_only:
             assert self.predict_normals
         
-        self.train_relight= True
+        #self.train_relight= True
         
         self.net_dict = nn.ModuleDict()
         self.embedder = self._init_embedder()
@@ -417,7 +417,7 @@ class RelightModule(nn.Module):
         depth_map       = result['depth_map']    # (1, nray)
         density_map     = result['density_map']  # (1, nray, 1)
         instance_map    = result['instance_map'] # (1, nray, nkeys)
-        surf_map             = result['surf']         # (1, nray, 3)
+        surf_map             = result['surf']         # (nray, 3)
         keys            = result['keys'] # list of keys
         latent_features = result['latent_features'] # dict of features per person
         #gt
@@ -497,7 +497,6 @@ class RelightModule(nn.Module):
             
             # Nx3 to 1xNx3
             xyz = surf_map[mask].reshape(1, -1, 3)
-            
             rayo = ray_o[mask]#.reshape(1, -1, 3)
             
             ret_mask[mask] = True
@@ -511,9 +510,9 @@ class RelightModule(nn.Module):
             
             # get the feature volume from the pretrained model
             with torch.no_grad():
-                nerf_net = self.net.models[key]
-                features, valid_mask = nerf_net.get_feature(xyz, sparse_features)
-                
+                nerf_net = self.net.model(key) 
+                features , valid_mask = nerf_net.get_feature(xyz, sparse_features)
+                #print('valid_mask: ', valid_mask.sum()/valid_mask.numel())
                 if xyz_noise is not None:
                     features_jitter, valid_mask_jitter  = nerf_net.get_feature(xyz_jittered, sparse_features)
                     
@@ -539,6 +538,7 @@ class RelightModule(nn.Module):
                 if normal_jitter is not None:
                     normal_jitter = torch.nn.functional.normalize(normal_jitter, p=2, dim=-1, eps=1e-7)
             else:
+                
                 normal_pred = normal_gt[mask]
                 normal_jitter = normal_pred
 
@@ -620,6 +620,42 @@ class RelightModule(nn.Module):
                 if rgb_olat is not None:
                     pred_olat[mask] = rgb_olat
         
+        
+        if self.predict_normals_lvis_only:
+                    
+            pred_normal = pred_normal[ret_mask]
+            pred_lvis = pred_lvis[ret_mask]
+            if xyz_noise is not None:
+                pred_normal_jitter = pred_normal_jitter[ret_mask]
+                pred_lvis_jitter = pred_lvis_jitter[ret_mask]
+                
+            normal = normal_gt[ret_mask]
+            lvis = lvis_hit_map[ret_mask]
+            
+            #####################################
+            #coords = batch['coord']
+            #import matplotlib.pyplot as plt
+            #blank = np.zeros((1080,1920,3)) 
+            #coords = coords[ret_mask].detach().cpu().numpy()
+            #
+            #
+            #values = (normal - pred_normal) 
+            #values = values.detach().cpu().numpy()
+            #print("values: ", values.shape)
+            #values = np.linalg.norm(values, axis=-1)**2
+            ##map 0 to green, 1 to red
+            #values = values / values.max()
+            #values = np.stack([values, 1-values, np.zeros_like(values)], axis=-1)
+            #blank[coords[:,0],coords[:,1]] = values
+            #
+            #plt.imshow(blank)
+            #plt.show()
+            #####################################
+            
+            pred = {'normal': pred_normal, 'lvis': pred_lvis}
+            gt = {'normal': normal, 'lvis': lvis, 'alpha': alpha_map[ret_mask]}
+            loss_kwargs = {'mode': mode, 'normal_jitter': pred_normal_jitter, 'lvis_jitter': pred_lvis_jitter}
+            return pred, gt, loss_kwargs
             
         
         if mode != 'train':
@@ -648,40 +684,7 @@ class RelightModule(nn.Module):
         
         
         
-        if self.predict_normals_lvis_only:
-            
-            
-            pred_normal = pred_normal[ret_mask]
-            pred_lvis = pred_lvis[ret_mask]
-            if xyz_noise is not None:
-                pred_normal_jitter = pred_normal_jitter[ret_mask]
-                pred_lvis_jitter = pred_lvis_jitter[ret_mask]
-                
-            normal = normal_gt[ret_mask]
-            lvis = lvis_hit_map[ret_mask]
-            
-            ####################################
-            #coords = batch['coord']
-            #import matplotlib.pyplot as plt
-            #blank = np.zeros((1080,1920)) 
-            #coords = coords[ret_mask].detach().cpu().numpy()
-            #
-            #
-            #values = (normal - pred_normal) 
-            #values = values.detach().cpu().numpy()
-            #print("values: ", values.shape)
-            #values = np.linalg.norm(values, axis=-1)**2
-            #
-            #blank[coords[:,0],coords[:,1]] = values
-            #
-            #plt.imshow(blank)
-            #plt.show()
-            ####################################
-            
-            pred = {'normal': pred_normal, 'lvis': pred_lvis}
-            gt = {'normal': normal, 'lvis': lvis, 'alpha': alpha_map[ret_mask]}
-            loss_kwargs = {'mode': mode, 'normal_jitter': pred_normal_jitter, 'lvis_jitter': pred_lvis_jitter}
-            return pred, gt, loss_kwargs
+        
             
         
         
@@ -803,7 +806,9 @@ class RelightModule(nn.Module):
         if relight_probes:
             rgb_probes = []
             for _, light in self.novel_probes.items():
-                rgb_relit = integrate(0.25*light + 0.1*self.light())
+                rgb_relit = integrate(0.35*light)
+                #rgb_relit = integrate(0.25*light + 0.1*self.light())
+
                 rgb_probes.append(rgb_relit)
             rgb_probes = torch.cat([x[:, None, :] for x in rgb_probes], dim=1)
         return rgb, rgb_olat, rgb_probes, hdr # Nx3